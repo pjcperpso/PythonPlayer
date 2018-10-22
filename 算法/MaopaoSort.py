6acccef789b1aa48d0c17230476aac440039d3d6
allen <1360652072@qq.com>
2018-10-22 21:00:00 +0800

    18/10/22--MaopaoSort

---
+++ 算法/MaopaoSort.py
@@ -0,0 +1,17 @@
+"""冒泡排序 Maopao"""
+def Maopao():
+    L = [1,5,4,3,9,8,2]
+    #临时存储变量
+    temp = 0
+    #单次排序
+    for x in range(1,len(L)):
+        #每次排序要比较的次数
+        for y in range(1,len(L)-x+1):
+            #如果后面的元素>前面的元素  则替换
+            if L[-y]>L[-y-1]:
+                temp = L[-y]
+                L[-y] = L[-y-1]
+                L[-y-1] = temp
+        print(L)
+
+Maopao()
---
+++ 算法/readme.txt
@@ -0,0 +1,2 @@
+The contents of this folder are some classic algorithms.
+这个文件夹中存放的内容为一些经典算法
