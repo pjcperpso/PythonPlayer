1fd47e03cb8d78f195a6da91783c63abe6e6cc99
allen <1360652072@qq.com>
2018-10-23 20:18:49 +0800

    18/10/23 pat:写出这个数

---
+++ PAT/PAT_乙级/写出这个数.py
@@ -0,0 +1,11 @@
+def write(i):
+    L = ['ling','yi','er','san','si','wu','liu','qi','ba','jiu']
+    chinese = str(i)
+    for x in range(0,len(chinese)-1):
+        sum = int(chinese[x])+int(chinese[x+1])
+        if sum>=1 and sum<=9:
+            print(L[sum])
+        else:
+            print('yi',L[sum%10])
+            
+write(152456156565456468654)
